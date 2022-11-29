412a7fb8abf751312853aef7f39bcd9895edea5e
Genoc1de <wavesshark@yandex.ru>
2022-11-29 23:32:50 +0500

    1

--- hotel/users/forms.py
+++ hotel/users/forms.py
@@ -2,14 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignUpForm(UserCreationForm):
     username = forms.CharField(label='Логин')
-    first_name = forms.CharField(label='Имя', max_length=30, required=False, help_text='По желанию.')
-    last_name = forms.CharField(label='Фамилия', max_length=30, required=False, help_text='По желанию.')
-    email = forms.EmailField(label='Электронная почта', max_length=254, help_text='Обязательно для заполнения. Введите действующий адрес электронной почты.')
+    first_name = forms.CharField(label='Имя', max_length=30, required=False, help_text='Необязательно')
+    last_name = forms.CharField(label='Фамилия', max_length=30, required=False, help_text='Необязательно')
+    email = forms.EmailField(label='Электронная почта', max_length=254,
+                             help_text='Обязательно для заполнения.Введите действующий адрес электронной почты.')
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Логин', max_length=30,)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput, max_length=30,)
+
--- hotel/users/views.py
+++ hotel/users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm
+from django.http import HttpResponse
+
 
 
 def signup(request):
@@ -16,3 +18,22 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})

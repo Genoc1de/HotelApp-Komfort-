39b92f9a9a4c6c3c73b9d0c835080c0bd0fdbc7f
Genoc1de <wavesshark@yandex.ru>
2022-11-29 22:31:46 +0500

    1

--- hotel/users/forms.py
+++ hotel/users/forms.py
@@ -1,13 +1,15 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = ('username', 'email',)
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(label='Логин')
+    first_name = forms.CharField(label='Имя', max_length=30, required=False, help_text='По желанию.')
+    last_name = forms.CharField(label='Фамилия', max_length=30, required=False, help_text='По желанию.')
+    email = forms.EmailField(label='Электронная почта', max_length=254, help_text='Обязательно для заполнения. Введите действующий адрес электронной почты.')
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput())
 
-class CustomUserChangeForm(UserCreationForm):
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email',)
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )

5ab76f1b206d3679e16a7252d79470835c2d1a3a
Genoc1de <wavesshark@yandex.ru>
2022-11-30 11:00:40 +0500

    1

--- hotel/articles/models.py
+++ hotel/articles/models.py
@@ -6,7 +6,6 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.title
@@ -17,10 +16,7 @@ class Article(models.Model):
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     comment = models.CharField(max_length=140)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+
 
     def __str__(self):
         return self.comment
--- hotel/home/models.py
+++ hotel/home/models.py
@@ -21,7 +21,7 @@ class Amenities(BaseModel):
 class Hotel(BaseModel):
     hotel_name= models.CharField(max_length=100)
     hotel_price = models.IntegerField()
-    description = models.TextField()
+    description = models.TextField(max_length=200)
     amenities = models.ManyToManyField(Amenities)
     room_count = models.IntegerField(default=10)
 

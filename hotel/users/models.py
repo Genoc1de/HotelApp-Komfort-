08720726964696f1bb464fa5e6a342ac43de8a30
Genoc1de <wavesshark@yandex.ru>
2022-12-06 11:28:01 +0500

    111

--- hotel/users/models.py
+++ hotel/users/models.py
@@ -17,6 +17,9 @@ class Profile(models.Model):
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
+            img_width = 300
+            img_height = 300
+            img = Image.open(self.image.path).resize((img_width, img_height))
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
